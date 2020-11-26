65e21339a78a334783acaabc34a62372f6a381e8
Fannie Ståhl <fannie.stahl@gmail.com>
2020-11-26 14:09:02 +0100

    bajs

--- coords.php
+++ coords.php
@@ -1,3 +1,33 @@
+<?php
+    include_once "/includes/validation.php";
+
+    if(isset($_POST["submit"])){
+        if(!isset($_POST["x"])||!Validator::isNumber($_POST["x"])){
+            die("x can´t be empty, can´t be non-numeric");
+        }
+        if(!isset($_POST["z"])||!Validator::isNumber($_POST["z"])){
+            die("z can´t be empty, can´t be non-numeric");
+        }
+        if(isset($_POST["y"])){
+            if(!Validator::isNumber($_POST["y"])) {
+                die("y can´t be non-numeric");
+            }
+        }
+
+        if(!isset($_POST["world"])||!in_array($_POST["world"],["overworld", "nether", "end"])){
+            die("You need to choose one of the three options");
+        }
+        if(!isset($_POST["location"])||!in_array($_POST["location"],["home", "biome", "temple","spawner", "misc"])){
+            die("You need to choose one of the options");
+        }
+        if(!isset($_POST["description"])){
+            die("You need to put in a description");
+        }
+        if(!isset($_POST["name"])){
+            die("You need to put in a location name");
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,15 +49,15 @@
 <body>
     <div class="container" id="inner-container">
         <h2>Add Point of Interest</h2>
+        <form method="POST" action="#">
         <div class="row">
             <div class="col-lg-4">
                 <!-- Input X Position -->
                 <div class="input-group mb-3">
-                    <div class="input-group-prepend">
+                    <div class="input-group-prepend" >
                         <span class="input-group-text" id="inputGroup-sizing-default">X</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="-216" aria-label="Default"
-                        aria-describedby="inputGroup-sizing-default">
+                    <input type="text" class="form-control" placeholder="-216" name="x">
                 </div>
             </div>
             <div class="col-lg-4">
@@ -36,8 +66,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroup-sizing-default">Y</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="76" aria-label="Default"
-                        aria-describedby="inputGroup-sizing-default">
+                    <input type="text" class="form-control" placeholder="76" name="y">
                 </div>
             </div>
             <div class="col-lg-4">
@@ -46,32 +75,31 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroup-sizing-default">Z</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="900" aria-label="Default"
-                        aria-describedby="inputGroup-sizing-default">
+                    <input type="text" class="form-control" placeholder="900" name="z">
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-lg-6">
                 <!-- Droppdown with worlds -->
-                <select class="form-control mb-3">
+                <select class="form-control mb-3" name="world">
                     <option>Select World</option>
                     <option>------------</option>
-                    <option>Overworld</option>
-                    <option>Nether</option>
-                    <option>End</option>
+                    <option value="overworld">Overworld</option>
+                    <option value="nether">Nether</option>
+                    <option value="end">End</option>
                 </select>
             </div>
             <div class="col-lg-6">
                 <!-- Droppdown for locations types -->
-                <select class="form-control mb-3">
+                <select class="form-control mb-3" name="location">
                     <option>Select Location Type</option>
                     <option>------------</option>
-                    <option>Home</option>
-                    <option>Biom</option>
-                    <option>Spawner</option>
-                    <option>Temple</option>
-                    <option>Misc</option>
+                    <option value="home">Home</option>
+                    <option value="biome">Biome</option>
+                    <option value="spawner">Spawner</option>
+                    <option value="temple">Temple</option>
+                    <option value="misc">Misc</option>
                 </select>
             </div>
         </div>
@@ -83,8 +111,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroup-sizing-default">Name of location</span>
                     </div>
-                    <input type="text" class="form-control" placeholder="Marqus house" aria-label="Default"
-                        aria-describedby="inputGroup-sizing-default">
+                    <input type="text" class="form-control" placeholder="Marqus house" name="name">
                 </div>
             </div>
             <div class="col-lg-6">
@@ -93,8 +120,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroup-sizing-default">Image</span>
                     </div>
-                    <input type="file" class="form-control" placeholder="img" aria-label="Default"
-                        aria-describedby="inputGroup-sizing-default">
+                    <input type="file" class="form-control" placeholder="img" name="img">
                 </div>
             </div>
         </div>
@@ -105,13 +131,14 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="inputGroup-sizing-default">Description</span>
                     </div>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" name="desciption"></textarea>
                 </div>
             </div>
         </div>
+    </form>
         <!--Button-->
         <div>
-            <button class="btn btn-light">Add</button>
+            <button class="btn btn-light" value="submit" name="submit">Add</button>
         </div>
     </div>
 
