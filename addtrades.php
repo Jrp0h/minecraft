544d3fa9c97808212f74dffbcc3b3e9bab5617ce
Marcus Nilsson <marcus.nilsson@genarp.com>
2020-12-07 11:05:59 +0100

    Added trades

--- addtrades.php
+++ addtrades.php
@@ -13,6 +13,96 @@
     $db = new Database();
     $locations = $db->query("SELECT * FROM points_of_interest WHERE world='Overworld'");
     $items = $db->query("SELECT * FROM items");
+
+    $errors = [];
+
+    if(isset($_POST["submit"])) {
+        if(!isset($_POST["type"])) {
+            die("SOMETHING WENT WRONG");
+        }
+
+        if(!isset($_POST["location"])) {
+            $errors["location"] = "You need to choose a location";
+        } else {
+            $location = $db->query("SELECT * FROM points_of_interest WHERE world='Overworld' AND id=:id", [ "id" => $_POST["location"] ]);
+
+            if(count($location) <= 0)
+                $errors['location'] = "You need to choose a location";
+        }
+
+        if($_POST["type"] == "trade") {
+
+            $hasSecondaryItem = false;
+
+            if(!isset($_POST["first_item"])) {
+                $errors["first_item"] = "You need to select an item";
+            } else {
+                $item = $db->query("SELECT * FROM items WHERE id=:id", [ "id" => $_POST["first_item"] ]);
+
+                if(count($item) <= 0)
+                    $errors['first_item'] = "You need to choose an item";
+            }
+
+            if(!isset($_POST["first_item_amount"]) || !Validator::isNumber($_POST["first_item_amount"]) || intval($_POST["first_item_amount"]) <= 0) {
+                $errors["first_item_amount"] = "Can't be empty and must be 1 or over";
+            }
+
+            if(isset($_POST["secondary_item"]) && $_POST["secondary_item"] != "") {
+                $item = $db->query("SELECT * FROM items WHERE id=:id", [ "id" => $_POST["secondary_item"] ]);
+
+                if(count($item) <= 0)
+                    $errors['secondary_item'] = "You need to choose an item";
+
+                if(!isset($_POST["secondary_item_amount"]) || !Validator::isNumber($_POST["secondary_item_amount"]) || intval($_POST["secondary_item_amount"]) <= 0) {
+                    $errors["secondary_item_amount"] = "Can't be empty and must be 1 or over";
+                }
+
+                $hasSecondaryItem = true;
+            }
+
+
+            if(!isset($_POST["return_item"])) {
+                $errors["return_item"] = "You need to select an item";
+            } else {
+                $item = $db->query("SELECT * FROM items WHERE id=:id", [ "id" => $_POST["return_item"] ]);
+
+                if(count($item) <= 0)
+                    $errors['return_item'] = "You need to choose an item";
+            }
+
+            if(!isset($_POST["return_item_amount"]) || !Validator::isNumber($_POST["return_item_amount"]) || intval($_POST["return_item_amount"]) <= 0) {
+                $errors["return_item_amount"] = "Can't be empty and must be 1 or over";
+            }
+
+            if(count($errors) <= 0) {
+                if($hasSecondaryItem)
+                {
+                    $db->exec("INSERT INTO trades(user_id, poi_id, item_id, item_amount, secondary_item_id, secondary_item_amount, return_item_id, return_item_amount) VALUES (:user_id, :poi_id, :item_id, :item_amount, :secondary_item_id, :secondary_item_amount, :return_item_id, :return_item_amount)", [
+                        "user_id" => Auth::userId(),
+                        "poi_id" => $_POST["location"],
+                        "item_id" => $_POST["first_item"],
+                        "item_amount" => $_POST["first_item_amount"],
+                        "secondary_item_id" => $_POST["secondary_item"], 
+                        "secondary_item_amount" => $_POST["secondary_item_amount"], 
+                        "return_item_id" => $_POST["return_item"], 
+                        "return_item_amount" => $_POST["return_item_amount"]
+                    ]);
+                }
+                else {
+                    $db->exec("INSERT INTO trades(user_id, poi_id, item_id, item_amount, return_item_id, return_item_amount) VALUES (:user_id, :poi_id, :item_id, :item_amount, :return_item_id, :return_item_amount)", [
+                        "user_id" => Auth::userId(),
+                        "poi_id" => $_POST["location"],
+                        "item_id" => $_POST["first_item"],
+                        "item_amount" => $_POST["first_item_amount"],
+                        "return_item_id" => $_POST["return_item"], 
+                        "return_item_amount" => $_POST["return_item_amount"]
+                    ]);
+                }
+            }
+        } else if($_POST["type"] == "enchantment") {
+
+        }
+    }
 ?>
 
 
@@ -43,90 +133,114 @@
             <h2>Add Emerald Trades</h2>
             <p><b>Put in what trades your villagers have to offer.</b></p>
            <!-- Location --> 
-            <div class="row">
-                <div class="col-lg-12">
-                    <!-- Droppdown with worlds -->
-                    <div class="form-group">
-                        <select class="form-control mb-1" name="location">
-                            <option value="">Select Location</option>
-                            <option value="">------------</option>
-                            <?php foreach ($locations as $l) : ?>
-                                <option value="<?php echo $l["id"]; ?>"><?php echo $l["name"]; ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-                </div>
-            </div>
-
-           <!-- First Item --> 
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <select class="form-control mb-1" name="first_item" data-custom-dropdown data-custom-dropdown-data="<?php echo base64_encode(json_encode($items)); ?>">
-                            <option value="">Select First Item</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <div class="input-group mb-1">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">Amount</span>
-                            </div>
-                            <input type="number" class="form-control" placeholder="1" name="first_item_amount">
+            <form method="POST">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <!-- Droppdown with worlds -->
+                        <div class="form-group">
+                            <select class="form-control mb-1" name="location">
+                                <option value="">Select Location</option>
+                                <option value="">------------</option>
+                                <?php foreach ($locations as $l) : ?>
+                                    <option value="<?php echo $l["id"]; ?>"><?php echo $l["name"]; ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <?php if(isset($errors['location'])): ?>
+                                <small class="text-danger"><?php echo $errors["location"]; ?></small>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
-            </div>
 
-           <!-- Secondary Item --> 
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <select class="form-control mb-1" name="secondary_item" data-custom-dropdown data-custom-dropdown-data="<?php echo base64_encode(json_encode($items)); ?>">
-                            <option value="">Select Secondary Item</option>
-                        </select>
+               <!-- First Item --> 
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <select class="form-control mb-1" name="first_item" data-custom-dropdown data-custom-dropdown-data="<?php echo base64_encode(json_encode($items)); ?>">
+                                <option value="">Select First Item</option>
+                            </select>
+                            <?php if(isset($errors['secondary_item_amount'])): ?>
+                                <small class="text-danger"><?php echo $errors["secondary_item_amount"]; ?></small>
+                            <?php endif; ?>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <div class="input-group mb-1">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">Amount</span>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <div class="input-group mb-1">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">Amount</span>
+                                </div>
+                                <input type="number" class="form-control" name="first_item_amount">
                             </div>
-                            <input type="number" class="form-control" placeholder="1" name="secondary_item_amount">
+                            <?php if(isset($errors['first_item_amount'])): ?>
+                                <small class="text-danger"><?php echo $errors["first_item_amount"]; ?></small>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
-            </div>
 
-           <!-- What you get --> 
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <select class="form-control mb-1" name="return_item" data-custom-dropdown data-custom-dropdown-data="<?php echo base64_encode(json_encode($items)); ?>">
-                            <option value="">Select What You Get</option>
-                        </select>
+               <!-- Secondary Item --> 
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <select class="form-control mb-1" name="secondary_item" data-custom-dropdown data-custom-dropdown-data="<?php echo base64_encode(json_encode($items)); ?>">
+                                <option value="">Select Secondary Item</option>
+                            </select>
+                            <?php if(isset($errors['secondary_item'])): ?>
+                                <small class="text-danger"><?php echo $errors["secondary_item"]; ?></small>
+                            <?php endif; ?>
+                        </div>
                     </div>
-                </div>
 
-                <div class="col-lg-6">
-                    <div class="form-group">
-                        <div class="input-group mb-1">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">Amount</span>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <div class="input-group mb-1">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">Amount</span>
+                                </div>
+                                <input type="number" class="form-control" name="secondary_item_amount">
                             </div>
-                            <input type="number" class="form-control" placeholder="1" name="result_item_amount">
+                            <?php if(isset($errors['secondary_item_amount'])): ?>
+                                <small class="text-danger"><?php echo $errors["secondary_item_amount"]; ?></small>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
-            </div>
-            <!--Button-->
-            <div>
-                <button class="btn btn-light">Add</button>
-            </div>
+
+               <!-- What you get --> 
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <select class="form-control mb-1" name="return_item" data-custom-dropdown data-custom-dropdown-data="<?php echo base64_encode(json_encode($items)); ?>">
+                                <option value="">Select What You Get</option>
+                            </select>
+                            <?php if(isset($errors['return_item'])): ?>
+                                <small class="text-danger"><?php echo $errors["return_item"]; ?></small>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <div class="input-group mb-1">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">Amount</span>
+                                </div>
+                                <input type="number" class="form-control" name="return_item_amount">
+                            </div>
+                            <?php if(isset($errors['return_item_amount'])): ?>
+                                <small class="text-danger"><?php echo $errors["return_item_amount"]; ?></small>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                <!--Button-->
+                <div>
+                    <input type="hidden" name="type" value="trade">
+                    <input type="submit" class="btn btn-light" name="submit" value="Add" >
+                </div>
+            </form>
         </div>
     </div>
 
